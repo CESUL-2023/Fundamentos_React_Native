a2692e1c86fd25dbeeb297326f30420e6d43ce84
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2023-08-08 11:16:09 -0300

    Entendo componentes e style inline

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: '#131016', padding: 24 }}>
+      <Text style={{ color: '#FDFCFE', fontSize: 24, fontWeight: 'bold', marginTop: 48 }} >Nome do Grupo</Text>
+      <Text style={{ color: '#6b6b6b', fontSize: 16, }} >Terça, 8 de Agosto de 2023.</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
