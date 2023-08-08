c8080df11688b8b30f4ca9995a9037de8207fc7d
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2023-08-08 11:20:18 -0300

    Ajustando estilo com StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#131016', padding: 24 }}>
-      <Text style={{ color: '#FDFCFE', fontSize: 24, fontWeight: 'bold', marginTop: 48 }} >Nome do Grupo</Text>
-      <Text style={{ color: '#6b6b6b', fontSize: 16, }} >Terça, 8 de Agosto de 2023.</Text>
+    <View style={styles.container}>
+      <Text style={styles.groupName} >Nome do Grupo</Text>
+      <Text style={styles.groupDate} >Terça, 8 de Agosto de 2023.</Text>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1, backgroundColor: '#131016', padding: 24
+  },
+  groupName: {
+    color: '#FDFCFE', fontSize: 24, fontWeight: 'bold', marginTop: 48
+  },
+  groupDate: {
+    color: '#6b6b6b', fontSize: 16,
+  }
+
+});
